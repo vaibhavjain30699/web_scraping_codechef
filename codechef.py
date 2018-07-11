3f1e7c6affe725ab919762341b6f6d6b4cef19bd
Vaibhav Jain <vaibhavjain30699@gmail.com>
2018-07-11 16:42:06 +0530

    Update codechef.py

--- codechef.py
+++ codechef.py
@@ -1,16 +1,19 @@
 import requests
 import bs4
 import string
+from os import system
 
 rating = input('Enter Your Codechefs Username: ')
-
 res = requests.get('https://www.codechef.com/users/' + rating)
-
 soup = bs4.BeautifulSoup(res.text, 'lxml')
-
 name = soup.select('h2')
 
-print('Name: ' + name[1].text)
+system('clear')
+
+if not name:
+	print('Name: ' + name[1])
+else:
+	print('No user found!')
 
 for i in soup.select('.rating-number'):
 	print('Your Rating is: ' + i.text)
